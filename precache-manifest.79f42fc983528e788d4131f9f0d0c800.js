1efc8e87b0aa77a149700a423a8232ca3a9c76d6
songee <songee@nexon.co.kr>
2020-08-10 10:58:03 +0900

    Updates

---
+++ precache-manifest.79f42fc983528e788d4131f9f0d0c800.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172e07bef57acdcd1ce6210966252620",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "f2db2743cff74a9186ad",
+    "url": "/movie-app/static/css/main.d7c71f90.chunk.css"
+  },
+  {
+    "revision": "dad3e4ff7b91ca5195c0",
+    "url": "/movie-app/static/js/2.e0e84a28.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/movie-app/static/js/2.e0e84a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2db2743cff74a9186ad",
+    "url": "/movie-app/static/js/main.0c8874d6.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  },
+  {
+    "revision": "c25c101302a2c4fa9593ff52fa3add3c",
+    "url": "/movie-app/static/media/star.c25c1013.svg"
+  },
+  {
+    "revision": "db39a923ade26f649d2113fcae0829b9",
+    "url": "/movie-app/static/media/star_border.db39a923.svg"
+  }
+]);
